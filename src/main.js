0759113e100ce347e4af39f6680c4213ea1e5da9
Maca Gonzalez <macarenapazgm@gmail.com>
2023-01-17 17:37:50 -0300

    hola

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+//numero de tarjeta
+let numbercard  = document.querySelector('.cardnumber');
+let numberInput = document.querySelector('#cardnumber');
+let numberErrordiv = document.querySelector('.error') ;
+
+//Ingreso dinamico del Number
