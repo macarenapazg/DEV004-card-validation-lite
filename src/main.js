492de3a000d79044f75ac422211b4b56734191d5
Maca Gonzalez <macarenapazgm@gmail.com>
2023-01-22 23:41:05 -0300

    finalizo algoritmo de Luhn y con ello primera parte proyecto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
 
-//numero de tarjeta
-let numbercard  = document.querySelector('.cardnumber');
-let numberInput = document.querySelector('#cardnumber');
-let numberErrordiv = document.querySelector('.error') ;
+// algoritmo de Luhn
+const algoritmoLuhn=(cardnumber) => {
+  let contador = 0; 
+  let luhnNumber = cardnumber.length;
 
-//Ingreso dinamico del Number
+  if(luhnNumber==16){
+      for(let i=0; i<luhnNumber; i++){
+          let arrayAdittion = parseInt(cardnumber[i]);
+  
+          if((i+2) % 2 == 0)
+             if(( arrayAdittion *=2) >9)
+                  arrayAdittion -=9;
+          contador += arrayAdittion;
+      }
+      return(contador % 10 ==0);
+  }
+  else{return(false);
+
+  }
+  
+} 
+// funcion de validación
+ const validate = () => {
+    let cardnumber = document.getElementById("cardnumber");
+    let result = document.getElementById("result");
+    let message="";
+
+    if (algoritmoLuhn(cardnumber.value)) {
+      message= "La tarjeta de crédito es válida :)";
+    }
+      // Show success in div#result
+    
+    else {message="La Tarjeta ingresada no es válida :("; 
+  }
+      // Show error message in div#result
+    result.textContent=message;
+  }
